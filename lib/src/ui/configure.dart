e7364b8c0e12a0ad935de5745bd9ec89611e3739
Jepc <joseenrique.pc21@gmail.com>
2022-11-30 09:59:03 -0400

    ADD configure colors

---
+++ lib/src/ui/configure.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+class Configure {
+  static const primary = Color(0xff71be69);
+  static const secondary = Color(0xff223f2e);
+  static const auxiliary = Color(0xffdfdfa1);
+  static const accent = Color(0xff0e140b);
+
+  static const primaryDark = Color(0xffb7da80);
+
+  static const primaryButtonLight = Color(0xff0e140b);
+  static const secondaryButtonLight = Color(0xffe4efef);
+
+  static const primaryButtonDark = primary;
+
+  static const backgroundLight = Color(0xffdddddd);
+  static const backgroundDark = primaryButtonLight;
+}
